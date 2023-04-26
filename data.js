083206b0d9562235a70a158d5af8ebaaddd5ff21
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-26 09:34:43 +0200

    add: JS files for future posts

---
+++ data.js
@@ -0,0 +1,24 @@
+export let posts = [
+    {
+        post_uuid: "",
+
+        heading : "",
+        post_photo: "./images/",
+        post_date: "",
+        intro_paragraph: "",
+
+        sections : [
+            {
+                heading: "",
+                paragraph: "",
+                paragraph_photo: "./images/"
+            },
+
+            {
+                heading: "",
+                paragraph: "",
+                paragraph_photo: "./images/"
+            }
+        ]
+    },
+]
