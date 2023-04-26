f22f5652344d9e805a4823f089898ec50cd48dd9
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-26 20:36:44 +0200

    add: view more to load all posts

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import {posts} from "./data.js"
 
 
-function displayFeaturedPost(){
+document.addEventListener("click", function(e){
+    if(e.target.id === "view-more"){
+        renderAllPostGrid()
+    }
+})
+
+
+
+function renderFeaturedPost(){
     const date = posts[0].post_date
     const heading = posts[0].heading
     const intro_paragraph = posts[0].intro_paragraph
@@ -22,28 +30,66 @@ function displayFeaturedPost(){
 }
 
 
-function displayPostGrid(){
-    const date = posts[1].post_date
-    const heading = posts[1].heading
-    const intro_paragraph = posts[1].intro_paragraph
-    const post_photo = posts[1].post_photo
+function renderFewPostGrid(){
+
     let posts_html = ""
+    posts.forEach(function(post){
 
-    posts_html = 
+        if(post.post_uuid != "0" && parseInt(post.post_uuid) < 4){
+            const date = post.post_date
+        const heading = post.heading
+        const intro_paragraph = post.intro_paragraph
+        const post_photo = post.post_photo
+    
+        posts_html += 
+    
+        `
+        <article>
+            <img class="post-img" src="${post_photo}">
+            <p class="date">${date}</p>
+            <h2 class="heading" >${heading}</h2>
+            <p class="intro-paragraph">${intro_paragraph}</p>
+        </article>
+        `
+        }
+    })
 
-    `
-    <article>
-        <img class="post-img" src="${post_photo}">
-        <p class="date">${date}</p>
-        <h2 class="heading" >${heading}</h2>
-        <p class="intro-paragraph">${intro_paragraph}</p>
-    </article>
-    `
+    posts_html += 
+        `
+        <p id="view-more" class="view-more" >View More</p>
+        `
+
+    document.getElementById("posts").innerHTML = posts_html
+}
+
+function renderAllPostGrid(){
+
+    let posts_html = ""
+    posts.forEach(function(post){
+
+        if(post.post_uuid != "0"){
+            const date = post.post_date
+        const heading = post.heading
+        const intro_paragraph = post.intro_paragraph
+        const post_photo = post.post_photo
+    
+        posts_html += 
+    
+        `
+        <article>
+            <img class="post-img" src="${post_photo}">
+            <p class="date">${date}</p>
+            <h2 class="heading" >${heading}</h2>
+            <p class="intro-paragraph">${intro_paragraph}</p>
+        </article>
+        `
+        }
+    })
 
     document.getElementById("posts").innerHTML = posts_html
 }
 
 
 
-displayFeaturedPost()
-displayPostGrid()
+renderFeaturedPost()
+renderFewPostGrid()
